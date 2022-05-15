24546e2748884bb1b3ca6bb61bd4a42376d89e14
ahmadg9az <ahmadg9az@gmail.com>
2022-05-16 02:47:33 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,49 @@
+
+import joblib
+from flask import Flask, render_template, request
+import preprocess
+import numpy as np 
+
+
+app = Flask(__name__)
+
+model = joblib.load('models/model.h5')
+scaler = joblib.load('models/scaler.h5')
+
+
+@app.route('/')
+def index() :
+    return render_template('index.html')
+
+
+@app.route('/predict', methods = ['POST', 'GET'])
+def get_prediction() :
+
+    if request.method == 'POST' :
+        year = request.form['Year']
+        gear_box = request.form['Gear Box']
+        horse_power = request.form['Horse Power']
+        kilometer = request.form['kilometer']
+        car_type = request.form['cartype']
+        car_model = request.form['model']
+        fuel_type = request.form['fuel_Type']
+
+    data = {'year' : year, 'gearbox' : gear_box, 'powerh' : horse_power, 
+            'kilometer' : kilometer, 'cartype' : car_type, 'model' : car_model, 'fueltype' : fuel_type}
+
+    final_data = preprocess.get_preprocess(data)
+
+    prediction = int(model.predict(scaler.transform([final_data]))[0])
+
+    return render_template('index.html', prediction_text=' The Price Is :{} $'.format(prediction))
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
---
+++ preprocess.py
@@ -0,0 +1,64 @@
+def gearbox (gear):
+    if gear.lower() == 'manual':
+        return [1]
+    elif gear.lower() == "auto":
+        return [0]
+
+
+
+def vehicle_Type (vtype):
+    if vtype.lower() == 'bus':
+        return [1,0,0,0,0,0,0]
+    elif vtype.lower() == 'cabrio':
+        return [0,1,0,0,0,0,0]
+    elif vtype.lower() == 'coupe':
+        return [0,0,1,0,0,0,0]
+    elif vtype.upper() == 'kleinwagen':
+        return [0,0,0,1,0,0,0]
+    elif vtype.upper() == 'limousine':
+        return [0,0,0,0,1,0,0]
+    elif vtype.lower()  == 'suv':
+        return [0,0,0,0,0,1,0]
+    elif vtype.lower() == 'van':
+        return [0,0,0,0,0,0,1]
+
+
+def car_model (model):
+    if model.lower() == 'beetle':
+        return [1,0,0,0,0,0,0]
+    elif model.lower() == 'golf':
+        return [0,1,0,0,0,0,0]
+    elif model.lower() == 'passat':
+        return [0,0,1,0,0,0,0]
+    elif model.lower() == 'polo':
+        return [0,0,0,1,0,0,0]
+    elif model.lower() == 'scirocco':
+        return [0,0,0,0,1,0,0]
+    elif model.lower() == 'tiguan':
+        return [0,0,0,0,0,1,0]
+    elif model.lower() == 'touran':
+        return [0,0,0,0,0,0,1]      
+
+
+
+def fuel_Type (ftype):
+    if ftype.lower() == 'diesel':
+        return [1,0,0,0]
+    elif ftype.lower() == 'electric':
+        return [0,1,0,0]
+    elif ftype.lower() == 'gasoline':
+        return [0,0,1,0]
+    elif ftype.lower() == 'hybrid':
+        return [0,0,0,1]
+
+
+def get_preprocess (data):
+    year= data['year']
+    gear= gearbox(data['gearbox'])
+    powerPS= data['powerh']
+    kilometer= data['kilometer']
+    vehicleType= vehicle_Type(data['cartype'])
+    model= car_model(data['model'])
+    fueltype= fuel_Type(data['fueltype'])
+    final_data= [ year , powerPS ,  kilometer ]  + gear + vehicleType + model + fueltype
+    return final_data   
